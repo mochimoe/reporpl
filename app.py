1eca6ab770b74fdee4c6f9f9223c650ecf8c8add
mochimoe <aisyahnyawn@gmail.com>
2018-09-23 21:31:32 +0700

    Update

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, request, abort
+from random import randint
 
 from linebot import (
     LineBotApi, WebhookHandler
@@ -59,12 +60,16 @@ def handle_message(event):
     sender = event.source.user_id #get usesenderr_id
     gid = event.source.sender_id #get group_id
     profile = line_bot_api.get_profile(sender)
-    if text=="adit":
-        line_bot_api.reply_message(event.reply_token,TextSendMessage(text='Kamu jahat adit'))
-    if text=="mail":
-        line_bot_api.reply_message(event.reply_token,TextSendMessage(text='Kamu jahat mail'))
-    if text=="tria":
-        line_bot_api.reply_message(event.reply_token,StickerSendMessage(package_id='1', sticker_id='1'))
+    def handle_message(event):
+    text = event.message.text #simplify for receove message
+    sender = event.source.user_id #get usesenderr_id
+    gid = event.source.sender_id #get group_id
+    profile = line_bot_api.get_profile(sender)
+    a=(randint(0, 9))
+    if a%2:
+        line_bot_api.reply_message(event.reply_token,TextSendMessage(text='Iya'))
+    else:
+        line_bot_api.reply_message(event.reply_token,TextSendMessage(text='Tidak'))
 
     line_bot_api.reply_message(event.reply_token,TextSendMessage(text='Halo '+profile.display_name+'\nKata Kunci Tidak Diketahui :) \nKetik "menu" untuk mengetahui menu yang tersedia'))
 
