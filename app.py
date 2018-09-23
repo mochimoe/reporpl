bd114b276bec8b5262d0b8fc72daaabce11c5c6c
mochimoe <aisyahnyawn@gmail.com>
2018-09-23 20:45:00 +0700

    Oke

---
+++ app.py
@@ -0,0 +1,74 @@
+from flask import Flask, request, abort
+
+from linebot import (
+    LineBotApi, WebhookHandler
+)
+from linebot.exceptions import (
+    InvalidSignatureError
+)
+from linebot.models import *
+import requests, json
+
+
+import errno
+import os
+import sys, random
+import tempfile
+import requests
+import re
+
+from linebot.models import (
+    MessageEvent, TextMessage, TextSendMessage,
+    SourceUser, SourceGroup, SourceRoom,
+    TemplateSendMessage, ConfirmTemplate, MessageAction,
+    ButtonsTemplate, ImageCarouselTemplate, ImageCarouselColumn, URIAction,
+    PostbackAction, DatetimePickerAction,
+    CarouselTemplate, CarouselColumn, PostbackEvent,
+    StickerMessage, StickerSendMessage, LocationMessage, LocationSendMessage,
+    ImageMessage, VideoMessage, AudioMessage, FileMessage,
+    UnfollowEvent, FollowEvent, JoinEvent, LeaveEvent, BeaconEvent,
+    FlexSendMessage, BubbleContainer, ImageComponent, BoxComponent,
+    TextComponent, SpacerComponent, IconComponent, ButtonComponent,
+    SeparatorComponent,
+)
+
+app = Flask(__name__)
+
+# Channel Access Token
+line_bot_api = LineBotApi('1URSFws7hTE4/3R5QWTFuYrMqU3x9fgkiL3y9AlHJcgb3X2j8WYZLhEXxQr2Taoczii6D3zHnWfsyRiiC6XqY9GRf79yRNkqz/c/oB/zsYFe+wzmbg6X9FUi/zGtVc0b/XJL2EnJ8ToGAl1XbC5fawdB04t89/1O/w1cDnyilFU=')
+# Channel Secret
+handler = WebhookHandler('7d787028825e0a82de4c61310da0c826')
+#===========[ NOTE SAVER ]=======================
+notes = {}
+
+# Post Request
+@app.route("/callback", methods=['POST'])
+def callback():
+    signature = request.headers['X-Line-Signature']
+    body = request.get_data(as_text=True)
+    app.logger.info("Request body: " + body)
+    try:
+        handler.handle(body, signature)
+    except InvalidSignatureError:
+        abort(400)
+    return 'OK'
+
+@handler.add(MessageEvent, message=TextMessage)
+def handle_message(event):
+    text = event.message.text #simplify for receove message
+    sender = event.source.user_id #get usesenderr_id
+    gid = event.source.sender_id #get group_id
+    profile = line_bot_api.get_profile(sender)
+    if text=="adit":
+        line_bot_api.reply_message(event.reply_token,TextSendMessage(text='Kamu jahat adit'))
+    if text=="mail":
+        line_bot_api.reply_message(event.reply_token,TextSendMessage(text='Kamu jahat mail'))
+    if text=="djohan":
+        line_bot_api.reply_message(event.reply_token,TextSendMessage(text='Kamu jahat djohan'))
+
+    line_bot_api.reply_message(event.reply_token,TextSendMessage(text='Halo '+profile.display_name+'\nKata Kunci Tidak Diketahui :) \nKetik "menu" untuk mengetahui menu yang tersedia'))
+
+import os
+if __name__ == "__main__":
+    port = int(os.environ.get('PORT', 5000))
+app.run(host='0.0.0.0', port=port)
