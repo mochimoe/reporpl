032bea3a7adc977b8ace1bcf57cd960d5b52056c
mochimoe <aisyahnyawn@gmail.com>
2018-10-12 15:56:46 +0700

    update

--- app.py
+++ app.py
@@ -17,6 +17,7 @@ import sys, random
 import tempfile
 import requests
 import re
+import requests, json
 
 from linebot.models import (
     MessageEvent, TextMessage, TextSendMessage,
@@ -43,6 +44,7 @@ handler = WebhookHandler('7d787028825e0a82de4c61310da0c826')
 notes = {}
 
 # Post Request
+
 @app.route("/callback", methods=['POST'])
 def callback():
     signature = request.headers['X-Line-Signature']
@@ -54,6 +56,53 @@ def callback():
         abort(400)
     return 'OK'
 
+def inputmhs(nrp, nama, jurusan):
+    r = requests.post("http://www.aditmasih.tk/api_aisyah/insert.php", data={'nrp': nrp, 'nama': nama, 'jurusan': jurusan})
+    data = r.json()
+
+    flag = data['flag']
+   
+    if(flag == "1"):
+        return 'Data '+nama+' berhasil dimasukkan\n'
+    elif(flag == "0"):
+        return 'Data gagal dimasukkan\n'
+
+def allmhs():
+    r = requests.post("http://www.aditmasih.tk/api_aisyah/all.php")
+    data = r.json()
+
+    flag = data['flag']
+   
+    if(flag == "1"):
+        hasil = ""
+        for i in range(0,len(data['data_sel'])):
+            nrp = data['data_sel'][int(i)][0]
+            nama = data['data_sel'][int(i)][2]
+            jurusan = data['data_sel'][int(i)][4]
+            hasil=hasil+str(i+1)
+            hasil=hasil+".\nNrp : "
+            hasil=hasil+nrp
+            hasil=hasil+"\nNama : "
+            hasil=hasil+nama
+            hasil=hasil+"\nJurusan : "
+            hasil=hasil+jurusan
+            hasil=hasil+"\n"
+        return hasil
+    elif(flag == "0"):
+        return 'Data gagal dimasukkan\n'
+
+#DELETE DATA MHS
+def hapusmhs(nrp):
+    r = requests.post("http://www.aditmasih.tk/api_hafid/delete.php", data={'nrp': nrp})
+    data = r.json()
+
+    flag = data['flag']
+   
+    if(flag == "1"):
+        return 'Data '+nrp+' berhasil dihapus\n'
+    elif(flag == "0"):
+        return 'Data gagal dihapus\n'
+
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
     text = event.message.text #simplify for receove message
@@ -61,13 +110,15 @@ def handle_message(event):
     gid = event.source.sender_id #get group_id
     profile = line_bot_api.get_profile(sender)
 
-    a=(randint(0, 9))
-    if a%2:
-        line_bot_api.reply_message(event.reply_token,TextSendMessage(text='Iya'))
-    else:
-        line_bot_api.reply_message(event.reply_token,TextSendMessage(text=' Tidak'))
+ if text=="Menu":
+        line_bot_api.reply_message(event.reply_token,TextSendMessage(text='Masukkan tambah NRP, NAMA, dan JURUSAN Selingkuhanmu untuk menambahkan data'))  
 
-
+        elif(data[0]=='tambah'):
+        line_bot_api.reply_message(event.reply_token, TextSendMessage(text=inputmhs(data[1],data[2],data[3])))
+        elif(data[0]=='hapus'):
+        line_bot_api.reply_message(event.reply_token, TextSendMessage(text=hapusmhs(data[1])))
+        elif(data[0]=='waifuku'):
+        line_bot_api.reply_message(event.reply_token, TextSendMessage(text=allsmhs()))
 import os
 if __name__ == "__main__":
     port = int(os.environ.get('PORT', 5000))
