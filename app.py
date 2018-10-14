16413a17723a3e6a79805a8be62c1ebb7b737889
mochimoe <aisyahnyawn@gmail.com>
2018-10-14 16:53:02 +0700

    h

--- app.py
+++ app.py
@@ -43,26 +43,26 @@ handler = WebhookHandler('7d787028825e0a82de4c61310da0c826')
 #===========[ NOTE SAVER ]=======================
 notes = {}
 
-def carimhs(nrp):
-    URLmhs = "http://www.aditmasih.tk/api_aisyah/show.php?nrp=" + nrp
-    r = requests.get(URLmhs)
-    data = r.json()
-    err = "data tidak ditemukan"
+# def carimhs(nrp):
+#     URLmhs = "http://www.aditmasih.tk/api_aisyah/show.php?nrp=" + nrp
+#     r = requests.get(URLmhs)
+#     data = r.json()
+#     err = "data tidak ditemukan"
 
-    flag = data['flag']
-    if(flag == "1"):
-        nrp = data['data_sel'][0]['nrp']
-        nama = data['data_sel'][0]['nama']
-        jurusan = data['data_sel'][0]['jurusan']
+#     flag = data['flag']
+#     if(flag == "1"):
+#         nrp = data['data_sel'][0]['nrp']
+#         nama = data['data_sel'][0]['nama']
+#         jurusan = data['data_sel'][0]['jurusan']
 
-    # munculin semua, ga rapi, ada 'u' nya
-    # all_data = data['data_angkatan'][0]
-    data = "Nama : "+nama+"\nNrp : "+nrp+"\nJurusan : "+jurusan
-    return data
-    # return all_data
+#     # munculin semua, ga rapi, ada 'u' nya
+#     # all_data = data['data_angkatan'][0]
+#     data = "Nama : "+nama+"\nNrp : "+nrp+"\nJurusan : "+jurusan
+#     return data
+#     # return all_data
 
-    elif(flag == "0"):
-        return err
+#     elif(flag == "0"):
+#         return err
 
 #input data
 def inputmhs(nrp, nama, jurusan):
